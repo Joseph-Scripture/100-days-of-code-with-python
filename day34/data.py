156d757ed84bfac1423573712479f9e811036a2b
Joseph-Scripture <joescript237@gmail.com>
2025-03-28 21:40:36 +0100

    Using beautifulSoup to do webscraping. scraped a site with the 100greatest movies of all time

--- day34/data.py
+++ day34/data.py
@@ -1,7 +1,7 @@
 import requests
 parameters = {
-    'amount':10,
-    'type':'boolean'
+    'amount': 10,
+    'type': 'boolean'
 }
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
---
+++ day44/main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+empire_movie =response.text
+
+
+soup = BeautifulSoup(empire_movie, 'html.parser')
+
+all_movies = soup.find_all(name='h2')
+movies_title = [movie.getText() for movie in all_movies]
+movies = movies_title[::-1]
+
+
+with open('day44/movies.txt', 'a') as folder:
+    
+    for movie in movies:
+        folder.write(f"{movie}\n")
+
+    # folder.write(title)
+    
